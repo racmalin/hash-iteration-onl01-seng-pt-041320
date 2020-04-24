5be9119afa94ae3300a6d07d3929698fc050fde7
Kevin C Malin <racmalin@yahoo.com>
2020-04-24 00:10:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -18,7 +18,7 @@ def age_appropriate_birthday(birthday_kids)
   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   else age < 12
     puts "You are too old for this!"
-end
+  end
 end
 
   
