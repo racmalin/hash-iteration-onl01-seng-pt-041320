88188b465ec0d7f9f124c49a1235a09e174043c4
Kevin C Malin <racmalin@yahoo.com>
2020-04-24 00:04:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,16 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+   if age < 12 
+   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+   else age < 12
+    # puts "You are too old for this!"
+ end
 end
+
+  
+
 
 
